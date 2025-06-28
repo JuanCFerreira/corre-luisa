8cd67e2712f5281127d1831990a49171d34747ee
JuanCFerreira <juancferreira.dev@gmail.com>
2025-06-27 21:53:35 -0300

    Fix image path in score update for consistency

--- js/entities/shell.js
+++ js/entities/shell.js
@@ -293,7 +293,7 @@ const ShellManager = {
       if (rectsCollide(Girl, s)) {
         this.shells.splice(i, 1);
         Game.score++;
-        document.getElementById('score').innerHTML = "<img width='30px' src='../sprites/coins/1.png'> " + Game.score;
+        document.getElementById('score').innerHTML = "<img width='30px' src='sprites/coins/1.png'> " + Game.score;
         
         // Tocar som de concha
         AudioManager.playSound('coinSound');
